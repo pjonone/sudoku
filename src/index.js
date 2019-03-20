2d316bb02fe26b3e412f74f43453d4f506c304c9
Sergey <44573680+pjonone@users.noreply.github.com>
2019-03-20 15:33:24 +0300

    fix: add recursion, change areas check without num

--- src/index.js
+++ src/index.js
@@ -1,53 +1,67 @@
 module.exports = function solveSudoku(matrix) {
-for (let row = 0; row < matrix.length; row++) {
-    for (let col = 0; col < matrix.length; col++) {
-        if (matrix[row][col] === 0) {
-            findResult(matrix, [row], [col]);
+    //решение через рекурсию
+    function findResult(matrix) {
+        for (let row = 0; row < 9; row++) {
+            for (let col = 0; col < 9; col++) {
+                if (matrix[row][col] === 0) { // нахожу значение с 0 и запускаю цикл подбора числа
+                    for (let num = 1; num <= 9; num++) {
+                        matrix[row][col] = num;   // временно ставлю число
+                        if (checkRow(matrix, row, col) && checkCol(matrix, row, col) && checkBox(matrix, row, col))   // проверка всех областей
+                            if (findResult(matrix))   // проверяю с новым числом следующие ячейки и в них следующие, если она проходит то отдаю тру и число записываю,
+                            // если нет, то возвращаюсь назад и убираю число
+                                return true;
+                    }
+                    matrix[row][col] = 0;
+                    return false;
+                }
+            }
         }
-    }
-}
-// решение
-function findResult(matrix, row, col) {
-    for (let num = 1; num <= 9; num++) {
-        if (checkRow(matrix, row, num) && checkCol(matrix, col, num) && checkBox(matrix, row, col, num)) { // проверяю число
-            matrix[row][col] =  num;
-        }
-    }
-   // findResult(matrix, [row], [col]);
-}
-// проверяю по полосе
-function checkRow(matrix, row, num) {
-    for (let col=0; col < 9; col++) {
-        if (matrix[row][col] == num) {
-            return false;
-        }
+        return true;
     }
 
-    return true;
-}
-//проверяю по колонке
-function checkCol(matrix, col, num) {
-    for (let row=0; row < 9; row++) {
-        if (matrix[row][col] == num) {
-            return false;
-        }
-    }
-
-    return true;
-}
-// проверяю по квадрату
-function checkBox(matrix, row, col, num) {
-    row = Math.floor(row / 3)*3;
-    col = Math.floor(col / 3)*3;
-
-    for (var r = 0; r < 3; r++) {
-        for (var c = 0; c < 3; c++) {
-            if (matrix[row + r][col + c] == num) {
+    // проверяю по полосе
+    function checkRow(matrix, row, col) {
+        for (let i = 0; i < 9; i++) {
+            if (col === i) { // пропускаем проверяемую ячейку
+                continue;
+            }
+            if (matrix[row][col] === matrix[row][i]) { // сравниваем другие числа в ячейках циклом с нашим числом
                 return false;
             }
         }
+        return true;
     }
-    return true;
-}
-  return matrix;
+
+    //проверяю по колонке
+    function checkCol(matrix, row, col) {
+        for (let i = 0; i < 9; i++) {
+            if (row === i) { // пропускаем проверяемую ячейку
+                continue;
+            }
+            if (matrix[row][col] === matrix[i][col]) { // сравниваем другие числа в ячейках циклом с нашим числом
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // проверяю по квадрату
+    function checkBox(matrix, row, col) {
+        let tempRow = Math.floor(row / 3) * 3; // находим начало квардата
+        let tempCol = Math.floor(col / 3) * 3;
+
+        for (let r = tempRow; r < tempRow + 3; r++) { // цикл от начала квадрата
+            for (let c = tempCol; c < tempCol + 3; c++) {
+                if ((r === row) && (c === col)) // пропускаем проверяемую ячейку
+                    continue;
+                if (matrix[r][c] === matrix[row][col]) { // сравниваем другие числа в ячейках циклом с нашим числом
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    findResult(matrix);
+    return matrix;
 }
